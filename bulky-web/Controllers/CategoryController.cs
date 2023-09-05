c15e8c5478e0e656e5949da5e75b87e137152056
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-05 09:09:53 -0400

    implemented client side validation.

--- bulky-web/Controllers/CategoryController.cs
+++ bulky-web/Controllers/CategoryController.cs
@@ -28,6 +28,11 @@ public class CategoryController : Controller
   [HttpPost]
   public IActionResult Create(Category obj)
   {
+    // if (obj.Name == obj.DisplayOrder.ToString()) // this is a custom validation
+    // {
+    //   ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+    // }
+
     if (ModelState.IsValid)
     {
       _db.Categories.Add(obj);
