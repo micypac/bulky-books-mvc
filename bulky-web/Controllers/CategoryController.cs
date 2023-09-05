ee65a28ddf1b4bd5f7371696bdf2c222ac0057b6
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-04 23:15:32 -0400

    first commit.

---
+++ bulky-web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using bulky_web.Data;
+using Microsoft.AspNetCore.Mvc;
+using bulky_web.Models;
+
+namespace bulky_web.Controllers;
+
+public class CategoryController : Controller
+{
+  private readonly ApplicationDbContext _db;
+
+  public CategoryController(ApplicationDbContext db)
+  {
+    _db = db;
+  }
+
+  public IActionResult Index()
+  {
+    List<Category> objCategoryList = _db.Categories.ToList();
+    return View(objCategoryList);
+  }
+}
---
+++ bulky-web/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bulky_web.Models;
+
+public class Category
+{
+  [Key]
+  public int Id { get; set; }
+  [Required]
+  public string Name { get; set; }
+  public int DisplayOrder { get; set; }
+}
