0ad88c6c7517bbe9c78fd58166be988a5f8352ca
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-05 11:30:33 -0400

    implemented update and delete operations for Categories.

--- bulky-web/Controllers/CategoryController.cs
+++ bulky-web/Controllers/CategoryController.cs
@@ -1,7 +1,7 @@
 ﻿using bulky_web.Data;
 using Microsoft.AspNetCore.Mvc;
 using bulky_web.Models;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data.Common;
 
 namespace bulky_web.Controllers;
 
@@ -41,5 +41,64 @@ public class CategoryController : Controller
     }
 
     return View(obj);
+  }
+
+  public IActionResult Edit(int? id)
+  {
+    if (id == null || id == 0)
+    {
+      return NotFound();
+    }
+
+    Category? retrievedCategory = _db.Categories.Find(id); // only works on primary keys
+    // Category? retrievedCategory1 = _db.Categories.FirstOrDefault(u => u.Id == id); // will return null if not found
+    // Category? retrievedCategory2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); // if need more filter criteria
+
+    if (retrievedCategory == null)
+    {
+      return NotFound();
+    }
+
+    return View(retrievedCategory);
+  }
+
+  [HttpPost]
+  public IActionResult Edit(Category obj)
+  {
+    if (ModelState.IsValid)
+    {
+      _db.Categories.Update(obj);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    return View(obj);
+  }
+
+  public IActionResult Delete(int? id)
+  {
+    if (id == null || id == 0)
+      return NotFound();
+
+    Category? retrievedCategory = _db.Categories.Find(id);
+
+    if (retrievedCategory == null)
+      return NotFound();
+
+    return View(retrievedCategory);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public IActionResult DeletePOST(int? id)
+  {
+    Category? obj = _db.Categories.Find(id);
+
+    if (obj == null)
+      return NotFound();
+
+    _db.Categories.Remove(obj);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+
   }
 }
