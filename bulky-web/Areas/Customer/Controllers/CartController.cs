ffdec69cc1c563469bf65778611766afe4143d6b
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-13 11:18:25 -0400

    implemented shopping cart UI.

--- bulky-web/Areas/Customer/Controllers/CartController.cs
+++ bulky-web/Areas/Customer/Controllers/CartController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using bulkyBook.Models;
+using bulkyBook.Models.ViewModels;
+using bulky.DataAccess.Repository.IRepository;
+using System.Security.Claims;
 
 namespace bulky_web.Areas.Customer.Controllers;
 
 [Area("Customer")]
+[Authorize]
 public class CartController : Controller
 {
+  private readonly IUnitOfWork _unitOfWork;
+
+  public ShoppingCartVM ShoppingCartVM { get; set; }
+
+  public CartController(IUnitOfWork unitOfWork)
+  {
+    _unitOfWork = unitOfWork;
+  }
+
   public IActionResult Index()
   {
-    return View();
+    var claimsIdentity = (ClaimsIdentity)User.Identity;
+    var userid = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+    ShoppingCartVM = new()
+    {
+      ShoppingCartList = _unitOfWork.ShoppingCart
+        .GetAll(item => item.ApplicationUserId == userid, includeProperties: "Product")
+    };
+
+    foreach (var cart in ShoppingCartVM.ShoppingCartList)
+    {
+      cart.Price = GetPriceBasedOnQuantity(cart);
+      ShoppingCartVM.OrderTotal += cart.Price * cart.Count;
+    }
+
+    return View(ShoppingCartVM);
+  }
+
+  private double GetPriceBasedOnQuantity(ShoppingCart cart)
+  {
+    if (cart.Count <= 50)
+      return cart.Product.Price;
+    else
+    {
+      if (cart.Count <= 100)
+        return cart.Product.Price50;
+      else
+        return cart.Product.Price100;
+    }
   }
 }
--- bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -6,14 +6,21 @@ namespace bulky.DataAccess.Repository.IRepository;
 public interface IRepository<T> where T : class
 {
   // T = Category or Product or Company etc
-  IEnumerable<T> GetAll(string? includeProperties = null);
+  IEnumerable<T> GetAll(
+    Expression<Func<T, bool>>? filter = null,
+    string? includeProperties = null
+  );
+
   T Get(
     Expression<Func<T, bool>> filter,
     string? includeProperties = null,
     bool tracked = false
   );
+
   void Add(T entity);
+
   void Remove(T entity);
+
   void RemoveRange(IEnumerable<T> entity);
 
 }
