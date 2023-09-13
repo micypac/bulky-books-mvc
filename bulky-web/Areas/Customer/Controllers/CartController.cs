c4d3e042d73d6f47861380aa0d406ae439ce92af
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-13 10:04:45 -0400

    created initial draft of shopping cart UI.

---
+++ bulky-web/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bulky_web.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class CartController : Controller
+{
+  public IActionResult Index()
+  {
+    return View();
+  }
+}
