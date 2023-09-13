4547674359902b7eacd9ba6cfcb95dba32eeb2a5
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-13 12:28:58 -0400

    created OrderHeader and OrderDetail models and updated db.

---
+++ bulky.Models/OrderDetail.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using bulkyBook.Models;
+
+namespace bulkyBook.Models;
+
+public class OrderDetail
+{
+  public int Id { get; set; }
+
+  [Required]
+  public int OderHeaderId { get; set; }
+  [ForeignKey("OrderHeaderId")]
+  [ValidateNever]
+  public OrderHeader OrderHeader { get; set; }
+
+  [Required]
+  public int ProductId { get; set; }
+  [ForeignKey("ProductId")]
+  [ValidateNever]
+  public Product Product { get; set; }
+
+  public int Count { get; set; }
+  public double Price { get; set; }
+}
