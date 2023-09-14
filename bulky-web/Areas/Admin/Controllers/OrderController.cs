26e6e9176965916c4be7ca24b8bb370be3b45616
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-14 08:06:30 -0400

    created OrderVM and OrderController for order admin ui.

---
+++ bulky-web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using bulkyBook.Models;
+using bulkyBook.Models.ViewModels;
+using bulky.DataAccess.Repository.IRepository;
+using bulky.Utility;
+
+namespace bulky_web.Areas.Admin.Controllers;
+
+public class OrderController : Controller
+{
+  private readonly IUnitOfWork _uow;
+
+  public OrderController(IUnitOfWork uow)
+  {
+    _uow = uow;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+
+
+  #region API CALLS
+
+  [HttpGet]
+  public IActionResult GetAll()
+  {
+    List<OrderHeader> listOfOrderHeaders = _uow.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+    return Json(new { data = listOfOrderHeaders });
+  }
+
+  #endregion
+}
---
+++ bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿using bulkyBook.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace bulkyBook.Models.ViewModels;
+
+public class OrderVM
+{
+  public OrderHeader OrderHeader { get; set; }
+  public IEnumerable<OrderDetail> OrderDetail { get; set; }
+}
