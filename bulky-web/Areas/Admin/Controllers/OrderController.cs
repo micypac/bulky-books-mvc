9f5f1de61b13af0bf6f40cf4a5bf85a23d18ae89
Michael Pacleb <michael.pacleb@gmail.com>
2023-09-14 15:19:59 -0400

    implemented UpdateOrderDetail POST action method.

--- bulky-web/Areas/Admin/Controllers/OrderController.cs
+++ bulky-web/Areas/Admin/Controllers/OrderController.cs
@@ -14,6 +14,9 @@ public class OrderController : Controller
 {
   private readonly IUnitOfWork _uow;
 
+  [BindProperty]
+  public OrderVM OrderVM { get; set; }
+
   public OrderController(IUnitOfWork uow)
   {
     _uow = uow;
@@ -26,13 +29,40 @@ public class OrderController : Controller
 
   public IActionResult Details(int orderId)
   {
-    OrderVM orderVM = new()
+    OrderVM = new()
     {
       OrderHeader = _uow.OrderHeader.Get(item => item.Id == orderId, includeProperties: "ApplicationUser"),
       OrderDetails = _uow.OrderDetail.GetAll(item => item.OrderHeaderId == orderId, includeProperties: "Product")
     };
 
-    return View(orderVM);
+    return View(OrderVM);
+  }
+
+  [HttpPost]
+  [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+  public IActionResult UpdateOrderDetail()
+  {
+    var orderHeaderFromDb = _uow.OrderHeader.Get(obj => obj.Id == OrderVM.OrderHeader.Id);
+
+    orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+    orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+    orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+    orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+    orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+    orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+
+    if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+      orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+
+    if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+      orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+
+    _uow.OrderHeader.Update(orderHeaderFromDb);
+    _uow.Save();
+
+    TempData["success"] = "Order Details Updated Successfully.";
+
+    return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
   }
 
 
